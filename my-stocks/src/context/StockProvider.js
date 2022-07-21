42e5905365e9ace10a7f59beb5f0177e7988894a
Polyanasousas <souzaspolyana@gmail.com>
2022-07-21 04:31:33 -0300

    feat: adicionados estados globais

--- my-stocks/src/context/StockProvider.js
+++ my-stocks/src/context/StockProvider.js
@@ -3,11 +3,30 @@ import PropTypes from 'prop-types';
 import StockContext from "./StockContext";
 
 const StockProvider = ({ children }) => {
-  const [stocksAPI, setStocksAPI] = useState([]);
+  const [selectedStock, setSelectedStock] = useState([]);
+  const [balanceValue, setBalanceValue] = useState(0);
+  const [balanceUpdateInput, setBalanceUpdateInput] = useState('');
+  const [stocksList, setStocksList] = useState([]);
+  const [usersData, setUsersData] = useState([]);
+  const [userEmail, setUserEmail] = useState('');
+  const [usersBalances, setUsersBalances] = useState([]);
+
 
   const context = {
-    stocksAPI,
-    setStocksAPI,
+    stocksList,
+    setStocksList,
+    selectedStock,
+    setSelectedStock,
+    balanceValue,
+    setBalanceValue,
+    balanceUpdateInput,
+    setBalanceUpdateInput,
+    usersData,
+    setUsersData,
+    userEmail,
+    setUserEmail,
+    usersBalances,
+    setUsersBalances,
   };
   return (
     <StockContext.Provider value={ context }>
