1a16f4573bc066ee16552ae70070bc749b570eb7
Polyanasousas <souzaspolyana@gmail.com>
2022-07-23 05:54:19 -0300

    feat: adicionado botão e texto de isntruçoes na pagina de cadastro

--- my-stocks/src/components/Registration.js
+++ my-stocks/src/components/Registration.js
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import StockContext from '../context/StockContext';
 import { validateEmailAndPassword } from '../helpers/Helpers';
 import stocks from '../db/data';
+import '../css/Registrate.css';
+import logo from '../images/logo-rectangle.png'
 
 const Registration = () => {
   let user = {};
@@ -16,6 +18,7 @@ const Registration = () => {
     setUserName,
     setBalanceValue,
     userPassword,
+    setUserPassword,
     stocksList,
     setStocksList,
     tableUpdated, 
@@ -36,16 +39,23 @@ const Registration = () => {
   }, []);
 
   const insertData = (input, inputValue) => {
+    if (input === 'email') setUserEmail(inputValue);
+    if (input === 'password') setUserPassword(inputValue);
+
     user[input] = inputValue;
     return user;
   }
 
-  const saveInStorage = (userObj) => {
+  const enableButton = () => {
     const loginValidation = validateEmailAndPassword(userEmail, userPassword);
-
+  
     if (!loginValidation) {
-      alert('Email ou senha inválido!')
+      return true;
     }
+    return false;
+  }
+
+  const saveInStorage = (userObj) => {
 
     tableUpdated ? setStocksList(stocksList) : setStocksList(stocksArr);
 
@@ -59,50 +69,75 @@ const Registration = () => {
   }
 
   return (
-    <>
+    <div id="registration-page">
+      <header>
+      <img
+          data-testid="registrate-logo-img"
+          src={ logo }
+          alt="Logotipo do site, escrito 'MyStocks com duas moedas do lado'"
+      />
+      </header>
+      <div id="instruction-container">
+        <p>Complete according to the following instructions:</p>
+        <ul>
+          <li>Fill in all required data</li>
+          <li>Add a valid email</li>
+          <li>Add a password with at least 8 characters (not special ones)</li>
+        </ul>
+      </div>
+      <div id="registrate-form-container">
       <form>
       <input
       type="text"
-      placeholder="Inform Your FirstName"
+      placeholder="  Inform Your FirstName"
       data-testid="firstname-input"
       onChange={ ({ target }) => insertData('firstName', target.value)}
       />
        <input
       type="text"
-      placeholder="Inform Your LastName"
+      placeholder="  Inform Your LastName"
       data-testid="lastname-input"
       onChange={ ({ target }) => insertData('lastName', target.value)}
       />
       <input
       type="text"
-      placeholder="Email"
+      placeholder="  Inform a document (CPF or Passport)"
+      data-testid="document-input"
+      />
+      <input
+      type="text"
+      placeholder="  Inform Account for transferences"
+      data-testid="tranfer-account-input"
+      onChange={ ({ target }) => insertData('transferAccount', target.value)}
+      />
+      <input
+      type="text"
+      placeholder="  Inform your email"
       data-testid="email-input"
       onChange={ ({ target }) => insertData('email', target.value)}
       />
       <input
       type="text"
-      placeholder="Password"
+      placeholder="  Inform your password"
       data-testid="password-input"
       onChange={ ({ target }) => insertData('password', target.value)}
       />
-      <input
-      type="text"
-      placeholder="CPF or Passport"
-      data-testid="document-input"
-      />
-      <input
-      type="text"
-      placeholder="Inform Account to transfer"
-      data-testid="tranfer-account-input"
-      onChange={ ({ target }) => insertData('transferAccount', target.value)}
-      />
+      <div id="registrate-button-container">
       <button
         type="button"
-        data-testid="save-button"
+        data-testid="return-login-button"
+        onClick={ () => navigate('/') }
+        >Return</button>
+      <button
+        type="button"
+        data-testid="save-registration-button"
+        disabled={ enableButton() }
         onClick={ () => saveInStorage(user) }
       >Conclude Registration</button>
-    </form>  
-    </>
+      </div>
+    </form> 
+    </div> 
+    </div>
   );
 }
 
