03fc8959724cdd0ac0e06a2ebcabc6fe30f6ba3c
Polyanasousas <souzaspolyana@gmail.com>
2022-07-23 05:52:05 -0300

    refactor: refatora estrutura de tags da pagina de login e importado imagens

--- my-stocks/src/pages/Login.js
+++ my-stocks/src/pages/Login.js
@@ -2,6 +2,10 @@ import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import StockContext from '../context/StockContext';
 import { checkUserSavedData} from '../helpers/Helpers';
+import '../css/Login.css'
+import logo from '../images/logo-square.png'
+import profile from '../images/usuario-de-perfil.png'
+import padlock from '../images/padlock.png'
 
 const Login = () => {
   const {
@@ -29,19 +33,45 @@ const Login = () => {
 
   
   return (
+    <div id="login-page">
+    <div id="login-logo-container">
+      <img
+          data-testid="login-logo-img"
+          src={ logo }
+          alt="Logotipo do site, escrito 'MyStocks com duas moedas do lado'"
+      />
+    </div>
+    <div id="login-form-container">
     <form>
+      <div id="login-inputs-container">
+        <div className="icon-input-container">
+      <img
+          data-testid="login-profile-icon"
+          src={ profile }
+          alt="Ícone de perfil"
+      />
       <input
         type="email"
         data-testid="email-input"
-        placeholder="Inform your email"
+        placeholder="  Inform your email"
         onChange={ ({ target }) => setUserEmail(target.value) }
+      />
+        </div>
+        <div className="icon-input-container">
+        <img
+          data-testid="login-padlock-icon"
+          src={ padlock }
+          alt="Ícone de cadeado"
       />
       <input
         type="password"
         data-testid="password-input"
-        placeholder="Inform your password"
+        placeholder="  Inform your password"
         onChange={ ({ target }) => setUserPassword(target.value) }
       />
+      </div>
+      </div>
+      <div id="login-buttons-container">
       <button
         type="button"
         data-testid="login-button"
@@ -56,8 +86,10 @@ const Login = () => {
       >
         Sign Up
       </button>
-      
+      </div>
     </form>
+    </div>
+    </div>
   );
 }
 
