354d20df55abaef2f11625ca672c05c130a40e3b
Polyanasousas <souzaspolyana@gmail.com>
2022-07-23 00:45:38 -0300

    feat: adicionado saldo e nome do usuário ao header

--- my-stocks/src/components/Header.js
+++ my-stocks/src/components/Header.js
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import StockContext from '../context/StockContext';
 
 const Header = () => {
+  const {
+    userName,
+    balanceValue,
+  } = useContext(StockContext);
+
   let navigate = useNavigate();
+
+  /* fla@email.com
+  flavinho */
 
   return (
     <div>
-      <header>       
-        <h2 data-testid="app-logo">Logo</h2>
-        <button
-          data-testid="general-btn"
-          type="button"
-          onClick={ () => navigate("/general") }
-        >
-        General
-        </button>
-        <button
-          data-testid="my-stocks-btn"
-          type="button"
-          onClick={ () => navigate("/my-stocks") }
-        >
-        My Stocks
-        </button>
-        <button
-          data-testid="account-btn"
-          type="button"
-          onClick={ () => navigate("/account") }
-        >
-        Account
-        </button>      
-        </header>
+      <header> 
+        <div>
+        <h3>Balance: R${+balanceValue}</h3>
+        <h3>Welcome, {userName}</h3>
+        </div>      
+        <div>
+          <h2 data-testid="app-logo">Logo</h2>
+          <button
+            data-testid="general-btn"
+            type="button"
+            onClick={ () => navigate("/general") }
+          >
+          General
+          </button>
+          <button
+            data-testid="my-stocks-btn"
+            type="button"
+            onClick={ () => navigate("/my-stocks") }
+          >
+          My Stocks
+          </button>
+          <button
+            data-testid="account-btn"
+            type="button"
+            onClick={ () => navigate("/account") }
+          >
+          Account
+          </button>      
+        </div>
+      </header>
     </div>
   );
 }
