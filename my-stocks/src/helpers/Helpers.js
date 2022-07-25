525cf5c07a3d6267b7a4a68a5f2eaa330427e637
Polyanasousas <souzaspolyana@gmail.com>
2022-07-24 22:55:05 -0300

    feat: funçoes pra atualizar tabela e saldo no storage

--- my-stocks/src/helpers/Helpers.js
+++ my-stocks/src/helpers/Helpers.js
@@ -8,12 +8,29 @@ export const checkUserSavedData = (userEmail, storageKey) => {
 
 export const validateEmailAndPassword = (email, password) => {
   const validateEmail = /^\w+@\w+\.\w+$/;
-  const validatePassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z$*&@#]{8,}$/;
+  const validatePassword = /^[0-9a-zA-Z]{8,}$/;
   
   if (!validateEmail.test(email) || !validatePassword.test(password)) {
-    return false;
+    return true;
   }
-  return true;
+  return false;
 };
+
+export const updateBalanceInStorage = (userEmail, newBalance, userName) => {
+  const balanceArr = JSON.parse(localStorage.getItem('balance'))
+        .filter(item => item.email !== userEmail)
+  const updatedBalanceArr = [...balanceArr, {value: newBalance, email: userEmail, name: userName}]
+    
+  localStorage.setItem('balance', JSON.stringify(updatedBalanceArr)) 
+}
+
+export const updateGeneralTable = (stocksList, updatedStock) => {
+  const list = stocksList;
+  const stockName = updatedStock.stock;
+  const findIndex = list.indexOf(list.find((el) => el.stock === stockName));
+
+  list[findIndex] = updatedStock;
+  return list;
+}
 
 
