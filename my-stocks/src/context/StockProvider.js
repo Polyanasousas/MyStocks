d55fe04babd309ee53f36df0ffdb3eec9f2253af
Polyanasousas <souzaspolyana@gmail.com>
2022-07-24 22:53:13 -0300

    feat: pagina de contexto global finalizada

--- my-stocks/src/context/StockProvider.js
+++ my-stocks/src/context/StockProvider.js
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import StockContext from "./StockContext";
+import stocks from '../db/data';
 
 const StockProvider = ({ children }) => {
   const [selectedStock, setSelectedStock] = useState([]);
-  const [balanceValue, setBalanceValue] = useState(0);
+  const [balanceValue, setBalanceValue] = useState(10000);
   const [balanceUpdateInput, setBalanceUpdateInput] = useState('');
-  const [stocksList, setStocksList] = useState([]);
   const [usersData, setUsersData] = useState([]);
   const [userEmail, setUserEmail] = useState('');
   const [usersBalances, setUsersBalances] = useState([]);
   const [userName, setUserName] = useState('');
   const [userPassword, setUserPassword] = useState('');
   const [tableUpdated, setTableUpdated] = useState(false);
-  const [myStocks, setMyStocks] = useState([])
-
+  const [myStocks, setMyStocks] = useState([]);
+  const [account, setAccount] = useState(0);
+  
+  const stocksNewArr = stocks.map((el) => ({
+    stock: el.cd_acao, 
+    company: el.nm_empresa_rdz,
+    price: el.vl_medio, 
+  }));
+    
+  const [stocksList, setStocksList] = useState(stocksNewArr);
 
   const context = {
     stocksList,
@@ -39,6 +47,8 @@ const StockProvider = ({ children }) => {
     setTableUpdated,
     myStocks, 
     setMyStocks,
+    account, 
+    setAccount,
   };
   
   return (
