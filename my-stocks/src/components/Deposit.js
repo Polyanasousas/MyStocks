5388b9bf747e0006c0010686a8aeb2a96a9766f4
Polyanasousas <souzaspolyana@gmail.com>
2022-07-24 22:51:29 -0300

    feat: pagina de depósito finalizada

--- my-stocks/src/components/Deposit.js
+++ my-stocks/src/components/Deposit.js
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import StockContext from '../context/StockContext';
+import { checkUserSavedData } from '../helpers/Helpers';
+import '../css/Transactions.css'
 
 const Deposit = () => {
   const {
@@ -13,37 +15,52 @@ const Deposit = () => {
     userName, 
     usersBalances,
     setUsersBalances,
+    account, 
+    setAccount,
   } = useContext(StockContext);
 
   let navigate = useNavigate();
+  const userAccount = checkUserSavedData(userEmail, 'user');
+
+  useEffect(() => {
+    userAccount ? setAccount(userAccount.transferAccount) : setAccount(0);
+  }, []);
 
 
-  const updateBalance = () => {
+  const balanceDeposit = () => {
     const newBalance = (+balanceValue + +balanceUpdateInput);
     const userObj = {value: newBalance, email: userEmail, name: userName};
 
     localStorage.setItem('balance', JSON.stringify([...usersBalances, userObj]));
     setUsersBalances([...usersBalances, userObj]);
     setBalanceValue(+newBalance);
-    navigate('/general');
+    navigate('/general')
   }
 
   return (
     <>
     <Header />
-    <h1>Deposit Page</h1>
-    <h2>Balance: R${balanceValue}</h2>
-    <input
-      type="text"
-      placeholder="Inform value"
-      data-testid="deposit-input"
-      onChange={ ({ target }) => setBalanceUpdateInput(target.value) }
-    />
-    <button
-      type="button"
-      data-testid="deposit-button"
-      onClick={ () => updateBalance() }
-    >Deposit</button>
+    <div className="transaction-page">
+      <div className="account-text-input-container">
+        <h3>Inform a value to deposit on your transfer account</h3>
+        <div className="account-number-container">
+          <span>{account}</span>
+        </div>
+      </div>
+      <form>
+        <input
+          type="number"
+          placeholder="  Inform value"
+          data-testid="deposit-input"
+          onChange={ ({ target }) => setBalanceUpdateInput(target.value) }
+        />
+        <button
+          type="button"
+          data-testid="deposit-button"
+          onClick={ () => balanceDeposit() }
+        >Deposit</button>
+      </form>
+      </div>
     </>
   );
 }
