a8f48987ba61c9b768002a805becfddd3e606dfa
Polyanasousas <souzaspolyana@gmail.com>
2022-07-24 22:51:56 -0300

    feat: headr finalizado

--- my-stocks/src/components/Header.js
+++ my-stocks/src/components/Header.js
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import StockContext from '../context/StockContext';
+import logo from '../images/logo-rectangle.png';
+import wallet from '../images/carteira.png'
+import '../css/Header.css';
 
 const Header = () => {
   const {
@@ -10,19 +13,31 @@ const Header = () => {
 
   let navigate = useNavigate();
 
-  /* fla@email.com
-  flavinho */
-
-  return (
-    <div>
-      <header> 
-        <div>
-        <h3>Balance: R${+balanceValue}</h3>
-        <h3>Welcome, {userName}</h3>
+    return (
+    <div id="general-header-container">
+      <header>
+        <div id="header-logo-container">
+        <img
+          data-testid="general-header-logo-img"
+          src={ logo }
+          alt="Logotipo do site, escrito 'MyStocks com duas moedas do lado"
+       />
+        </div>
+        <div id="header-text-container">
+          <h3>Hi, {userName}!</h3>
+            <div id="header-icon-balance-container">
+            <img
+              id="header-icon"
+              data-testid="generalheader-icon-img"
+              src={ wallet }
+              alt="ícone de carteira"
+            />
+            <h3>R${Number(balanceValue).toFixed(2)}</h3>
+            </div>
         </div>      
-        <div>
-          <h2 data-testid="app-logo">Logo</h2>
+        <div id="header-page-buttons">
           <button
+            className='account-border'
             data-testid="general-btn"
             type="button"
             onClick={ () => navigate("/general") }
@@ -30,6 +45,7 @@ const Header = () => {
           General
           </button>
           <button
+            className='button-border'
             data-testid="my-stocks-btn"
             type="button"
             onClick={ () => navigate("/my-stocks") }
@@ -37,6 +53,7 @@ const Header = () => {
           My Stocks
           </button>
           <button
+            className='button-border'
             data-testid="account-btn"
             type="button"
             onClick={ () => navigate("/account") }
