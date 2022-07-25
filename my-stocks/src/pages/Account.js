43758bf8b4dafe3149e4e5bbcbf7984ae1705671
Polyanasousas <souzaspolyana@gmail.com>
2022-07-24 23:00:04 -0300

    feat: pagina account completa

--- my-stocks/src/pages/Account.js
+++ my-stocks/src/pages/Account.js
@@ -3,15 +3,14 @@ import Header from '../components/Header';
 import { Link } from 'react-router-dom';
 import depositImage from '../images/caixa-de-deposito.png';
 import withdrawImage from '../images/saque.png';
-import historicImage from '../images/extrato-bancario.png';
-import '../css/account-cards.css'
+import '../css/Account.css'
 
 const Account = () => {
   return (
     <>
+    <div id="account-page-container">
     <Header />
-    <div>Balance Available: R$</div>
-    <section className='cards-container'>
+    <section className='account-cards-container'>
       <div className='account-card'>
         <Link to='/account/deposit'>
           <div className='card-img-container'>
@@ -21,7 +20,7 @@ const Account = () => {
               width="100%"
               alt='Desenho de uma mão depositando moeda numa caixa'
               className="account-card-img"
-            />
+              />
           </div>
         </Link>
         <div className='card-name-container'>
@@ -37,31 +36,16 @@ const Account = () => {
               width="100%"
               alt='Desenho de uma mão recebendo moedas'
               className="account-card-img"
-            />
+              />
           </div>
         </Link>
         <div className='card-name-container'>
           <h2>Withdraw</h2>
         </div>
       </div>
-      <div className='account-card'>
-        <Link to='/account/historic'>
-          <div className='card-img-container'>
-            <img
-              data-testid='historic-card-image'
-              src={ historicImage }
-              width="100%"
-              alt='Desenho de um extrato bancário com um cifrão na frente'
-              className="account-card-img"
-            />
-          </div>
-        </Link>
-        <div className='card-name-container'>
-          <h2>Historic</h2>
-        </div>
-      </div>
     </section>
-    </>
+    </div>
+              </>
   );
 }
 
