b5eb3dd86f4feeec92c5e92e5144c04222804b5c
Polyanasousas <souzaspolyana@gmail.com>
2022-07-24 23:00:35 -0300

    feat: pagina general completa

--- my-stocks/src/pages/General.js
+++ my-stocks/src/pages/General.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import StockContext from '../context/StockContext';
 import Header from '../components/Header';
+import '../css/General.css'
 
 const General = () => {
   const {
@@ -11,30 +12,36 @@ const General = () => {
 
   let navigate = useNavigate();
 
-  const tableTitles = ['Stock', 'Company',  'Unit Value(BRL)', 'Avaiable Qnt']
+  const tableTitles = ['Stock', 'Unit Value(BRL)', 'Trade'];
 
-  const selectStock = (stock) => {
-    setSelectedStock(stock)
+  const keysForTable = stocksList.map(({ stock, price }) => ({stock , price}));
+    
+
+  const selectStock = (stockSelected) => {
+    const stockToTrade = stocksList
+      .find((el) => el.stock === stockSelected.stock);
+    setSelectedStock(stockToTrade);
     navigate("/trade")
   }
 
   return (
     <>
     <Header />
-      <table border="1">
+      <table border="1" id="general-page-table">
         <thead>
           <tr>
             {tableTitles.map((title) => <th key={ title }>{ title }</th>)}
           </tr>
         </thead>
         <tbody>
-          {stocksList.map((item) => (
+          {keysForTable.map((item) => (
             <tr key={ item.stock }>
-              {Object.values(item).map((el) => (<td key={ el }>{ el }</td>))}
+              {Object.values(item).map((el) => (<td className="general-page-td"key={ el }>{ el }</td>))}
               <td>
                 <button
+                className="general-trade-button"
                 type="button"
-                data-testid="login-button"
+                data-testid="geral-table-button"
                 onClick={ () => selectStock(item) }
                 >Trade</button></td>
             </tr>))}
